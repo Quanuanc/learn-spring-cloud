ea060fc3903387bc4da73a917fb826341af00892
Quanuanc <quanuanc@outlook.com>
2023-03-02 00:39:42 +0800

    P09

--- service-borrow/src/main/java/cheng/service/impl/BorrowServiceImpl.java
+++ service-borrow/src/main/java/cheng/service/impl/BorrowServiceImpl.java
@@ -6,7 +6,6 @@ import cheng.entity.User;
 import cheng.entity.UserBorrowDetail;
 import cheng.mapper.BorrowMapper;
 import cheng.service.BorrowService;
-
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -19,18 +18,19 @@ public class BorrowServiceImpl implements BorrowService {
 
     @Resource
     BorrowMapper mapper;
+    @Resource
+    RestTemplate template;
 
     @Override
     public UserBorrowDetail getUserBorrowDetailByUid(int uid) {
         List<Borrow> borrow = mapper.getBorrowsByUid(uid);
-        RestTemplate template = new RestTemplate();
         //这里通过调用getForObject来请求其他服务，并将结果自动进行封装
         //获取User信息
-        User user = template.getForObject("http://localhost:8031/user/"+uid, User.class);
+        User user = template.getForObject("http://userservice/user/" + uid, User.class);
         //获取每一本书的详细信息
         List<Book> bookList = borrow
                 .stream()
-                .map(b -> template.getForObject("http://localhost:8011/book/"+b.getBid(), Book.class))
+                .map(b -> template.getForObject("http://bookservice/book/" + b.getBid(), Book.class))
                 .collect(Collectors.toList());
         return new UserBorrowDetail(user, bookList);
     }
