f4434bc67700d6eaad6597a6a62c1f740026cf4b
Quanuanc <quanuanc@outlook.com>
2023-03-05 16:18:10 +0800

    Finish P23 Nacos 配置中心
    使用@RefreshScope可以自动刷新配置

--- service-user/src/main/java/cheng/controller/UserController.java
+++ service-user/src/main/java/cheng/controller/UserController.java
@@ -4,6 +4,8 @@ import cheng.entity.User;
 import cheng.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,16 +13,21 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 
 @RestController
+@RefreshScope
 public class UserController {
     Logger log = LoggerFactory.getLogger(getClass());
 
     @Resource
     UserService service;
 
+    @Value("${test.config1}")
+    String hello;
+
     //这里以RESTFul风格为例
     @RequestMapping("/user/{uid}")
     public User findUserById(@PathVariable("uid") int uid) {
         log.info("调用了user服务");
+        log.info("config1 value: {}", hello);
         return service.getUserById(uid);
     }
 }
