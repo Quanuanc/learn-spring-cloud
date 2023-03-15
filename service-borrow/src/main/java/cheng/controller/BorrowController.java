aa16e6430ba9dac9fbd1a38a3200e6d615c9e2d2
Quanuanc <quanuanc@outlook.com>
2023-03-16 00:12:43 +0800

    Finish P31

--- service-borrow/src/main/java/cheng/controller/BorrowController.java
+++ service-borrow/src/main/java/cheng/controller/BorrowController.java
@@ -2,6 +2,9 @@ package cheng.controller;
 
 import cheng.entity.UserBorrowDetail;
 import cheng.service.BorrowService;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +18,16 @@ public class BorrowController {
     BorrowService service;
 
     @RequestMapping("/borrow/{uid}")
-    UserBorrowDetail findUserBorrows(@PathVariable("uid") int uid){
+    UserBorrowDetail findUserBorrows(@PathVariable("uid") int uid) {
         return service.getUserBorrowDetailByUid(uid);
+    }
+
+    @GetMapping("/blocked")
+    JSONObject blocked() {
+        JSONObject result = new JSONObject();
+        result.put("code", HttpStatus.FORBIDDEN);
+        result.put("success", false);
+        result.put("message", "您的请求频率过快，请稍后重试");
+        return result;
     }
 }
